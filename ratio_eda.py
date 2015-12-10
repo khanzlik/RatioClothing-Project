2dfc2d20b57ca2d3c1f25e3ef2e42c2ccd14be4b
khanzlik <knhanzlik@gmail.com>
2015-12-10 14:26:50 -0700

    dropped more columns

--- ratio_eda.py
+++ ratio_eda.py
@@ -2,15 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from sklearn.cross_validation import train_test_split,\
-cross_val_score
-from functions import *
+from sklearn.cross_validation import train_test_split, cross_val_score
 from sklearn import datasets, linear_model
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
 from sklearn.linear_model import Ridge, Lasso
-from sklearn.ensemble import RandomForestRegressor,\
-GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error as mse
 
 def dummies(df, column_list):
@@ -35,7 +32,8 @@ if __name__=='__main__':
 	drop_columns = ['created_at', 'updated_at', 'source', 'birthday_month',\
 	'posture', 'watch_wrist', 'watch_size', 'rise_inches', 'pocket_size',\
 	'lastorderdate', 'shoulder_left', 'shoulder_right', 'shoulder_slope',\
-	'estimated_birth_year', 'button_count', 'button_stance']
+	'estimated_birth_year', 'button_count', 'button_stance', 'posture_alteration',\
+	'lastorderid']
 	df.drop(drop_columns, axis=1, inplace=True)
 
 	df = dummies(df, 'build')
@@ -43,12 +41,12 @@ if __name__=='__main__':
 	df = dummies(df, 'sleeve_fit')
 	df = dummies(df, 'tuck')
 	df = dummies(df, 'fit')
-	drop_dummies = ['Average', 'Standard', 'No Pleats', 'Both', 'Traditional']
+	drop_dummies = ['Average','No Pleats',  'Standard', 'Both', 'Traditional']
 	df.drop(drop_dummies, inplace=True, axis=1)
 
+	df.to_csv('data/cleaned_ratio_data')
+
 	# df.describe()
-	df = df[df['height_inches']<100.0]
-	df = df[df['age_years']>2]
 
 	column_list = ['height_inches', 'age_years', 'weight_pounds']
 	histogram(df, column_list, bins=50)
