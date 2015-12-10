5a962604c919ff10b172500b060f6c585ab80b38
khanzlik <knhanzlik@gmail.com>
2015-12-10 14:26:36 -0700

    worked on model evaluation

--- models.py
+++ models.py
@@ -1,23 +1,52 @@
 from functions import *
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn import linear_model
+from sklearn.cross_validation import train_test_split, cross_val_score
+from sklearn import preprocessing
+from sklearn.metrics import mean_squared_error as mse
 
 df = pd.read_csv('data/cleaned_ratio_data')
 
 '''
-Erics Model
+Ratio's Current Model
 '''
-
-def neck(tshirt, height_inches, weight_pounds):
-	necksize = .2104 * tshirt + -0.0874 * height_inches + 0.0216 *\
-	weight_pounds + 17.2057
-	return necksize
-
-def sleeve(tshirt, height_inches, weight_pounds, build=None, suit=None, inseam=None):
-	if not build and not suit and not inseam:
-		sleevesize = 0.1425 * tshirt + 0.1835 * height_inches + 0.0073 *\
-		weight_pounds + 1.303
+def model_evaluation(model, features, target, y_test, x_test, test_x=None):
+	model.fit(features, target)
+	intercept = model.intercept_
+	coef = np.hstack([intercept, model.coef_])
+	if test_x is not None:
+		prediction = model.predict(test_x)
+		performance = mse(y_test, model.predict(X_test))
+		return coef, prediction, performance
 	else:
-		sleevesize = 0.1859 * tshirt + 0.3179 * height_inches + 0.0048 *\
-		weight_pounds + 9.5991
-	return sleevesize
+		return coef
+ 
 
-# changing build to 1/2/3/4
+if __name__ == '__main__':
+
+	columns = df[['t_shirt_size', 'height_inches', 'weight_pounds', 'neck']]
+	columns.dropna(inplace=True)
+	y = columns.pop('neck').values 
+	X = columns.values
+	X_train, X_test, y_train, y_test = train_test_split(X, y,\
+  	random_state=1, test_size=.1)
+	model = linear_model.LinearRegression() 
+	test_x = np.array([[9, 70, 210]])
+	ratio = model_evaluation(model, X_train, y_train, y_test, X_test, test_x)
+	# (array([ 16.11502377,   0.187813  ,  -0.06965918,   0.02083254]),\
+		# array([ 17.30403131]), mse =  0.31745101549213006)
+
+# lm = linear_model.LinearRegression()
+# neckfeatures = df[['t_shirt_size', 'height_inches', 'weight_pounds']]
+# test_x = np.array([[9, 70, 210]])
+# model_evaluation(lm, neckfeatures, df['neck'], test_x)
+
+
+# 12 target variables
+# if they supply shirt_neck_inches, we don't predict neck
+# changing build to 1/2/3/4?, fit preference?
+# build=None=Average/suitlength=None=0/tuck=None=0
+# incoporating measurements from store
