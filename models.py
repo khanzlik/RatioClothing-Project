6cb9530cdb17c5ca89ff7801cc2c4c0e20e97726
khanzlik <knhanzlik@gmail.com>
2015-12-11 13:11:26 -0700

    changing train/test values

--- models.py
+++ models.py
@@ -11,29 +11,26 @@ from sklearn.decomposition import PCA
 from sklearn.linear_model import RidgeCV, LassoCV
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 
-def load_clean_data(file_loc, columns=None, target=None):
-	df = pd.read_csv(file_loc)
-	if columns is not None and target is not None:
-		df = df[columns]
-		df.dropna(inplace=True)
-		y = df.pop(target).values 
-		X = df.values
-		X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=.1)
-	  	return df, X_train, X_test, y_train, y_test
-	else:
-		return df
-
-def model_evaluation(model, features, target, y_test, X_test, survey=None):
+def model_evaluation(model, features, target, y_test, X_test):
 	model.fit(features, target)
 	intercept = model.intercept_
 	coef = np.hstack([intercept, model.coef_])
-	if survey is not None:
-		prediction = model.predict(survey)
+	if X_test is not None:
+		prediction = model.predict(X_test)
 		mean_squared_error = mse(y_test, model.predict(X_test))
 		variance = model.score(X_test, y_test)
 		return coef, prediction, mean_squared_error, variance
 	else:
 		return coef
+
+def ridge_lasso(model, X_train, y_train, y_test, X_test):
+	standardizer = preprocessing.StandardScaler().fit(X_train)
+  	X_train = standardizer.transform(X_train)
+  	X_test = standardizer.transform(X_test)
+  	if model == ridge:
+  		return model_evaluation(ridge, X_train, y_train, y_test, X_test)
+  	elif model == lasso:
+  		return model_evaluation(lasso, X_train, y_train, y_test, X_test)
 
 if __name__ == '__main__':
 
@@ -42,50 +39,75 @@ if __name__ == '__main__':
 	'''
 	Neck
 	'''
-	columns = ['t_shirt_size', 'height_inches', 'weight_pounds', 'neck']
-	df, X_train, X_test, y_train, y_test = load_clean_data(file_loc, columns, 'neck')
+	columns = ['t_shirt_size', 'height_inches', 'weight_pounds', 'shirt_neck_inches']
+	mask_train = pd.notnull(df['shirt_neck_inches'])
+	X_train = df[columns][mask_train]
+	y_train = X_train.pop('shirt_neck_inches')
+	mask_test = pd.notnull(df['neck_inches'])
+	X_test = df[columns][mask_test]
+	y_test = X_test.pop('neck_inches')
 	linear = linear_model.LinearRegression()
-	survey = np.array([[9, 70, 210]])
-	ratio_eval_neck = model_evaluation(linear, X_train, y_train, y_test, X_test, survey)
+	ratio_eval_neck = model_evaluation(linear, X_train, y_train, y_test, X_test)
 	# mse =  0.31745101549213006; R2 = 71746544559006742
-
-	standardizer = preprocessing.StandardScaler().fit(X_train)
-  	X_train = standardizer.transform(X_train)
-  	X_test = standardizer.transform(X_test)
 
   	alphas = np.logspace(-3, 1)
 	ridge = RidgeCV(alphas=alphas)
-	ridge_eval = model_evaluation(ridge, X_train, y_train, y_test, X_test, survey)
+	ridge_eval_neck = ridge_lasso(ridge, X_train, y_train, y_test, X_test)
 	lasso = LassoCV()
-	lasso_eval = model_evaluation(lasso, X_train, y_train, y_test, X_test, survey)
+	lasso_eval_neck = ridge_lasso(lasso, X_train, y_train, y_test, X_test)
 
 	'''
 	Sleeve
 	'''
 	columns = ['t_shirt_size', 'height_inches', 'weight_pounds', 'Fit',
-	'Full', 'Muscular', 'jacket_size', 'jacket_length', 'pant_inseam_inches', 'sleeve']
-	df, X_train, X_test, y_train, y_test = load_clean_data(file_loc, columns, 'sleeve')
-	survey = np.array([[6, 76, 185, 1, 0, 0, 41, 1, 33]])
-	ratio_eval_sleeve = model_evaluation(linear, X_train, y_train, y_test, X_test, survey)
+	'Full', 'Muscular', 'jacket_size', 'jacket_length', 'pant_inseam_inches', 'shirt_sleeve_inches']
+	df, X_train, X_test, y_train, y_test = load_clean_data(file_loc, columns, 'shirt_sleeve_inches')
+	ratio_eval_sleeve = model_evaluation(linear, X_train, y_train, y_test, X_test)
 
-	standardizer = preprocessing.StandardScaler().fit(X_train)
-  	X_train = standardizer.transform(X_train)
-  	X_test = standardizer.transform(X_test)
+	ridge_eval_sleeve = ridge_lasso(ridge, X_train, y_train, y_test, X_test)
+	lasso_eval_sleeve = ridge_lasso(lasso, X_train, y_train, y_test, X_test)
 
-	ridge_eval_2 = model_evaluation(ridge, X_train, y_train, y_test, X_test, survey)
-	lasso_eval_2 = model_evaluation(lasso, X_train, y_train, y_test, X_test, survey)
+	columns = ['t_shirt_size', 'height_inches', 'weight_pounds', 'shirt_sleeve_inches']
+	df, X_train, X_test, y_train, y_test = load_clean_data(file_loc, columns, 'shirt_sleeve_inches')
+	ratio_eval_sleeve2 = model_evaluation(linear, X_train, y_train, y_test, X_test)
 
-	columns = ['t_shirt_size', 'height_inches', 'weight_pounds', 'sleeve']
-	df, X_train, X_test, y_train, y_test = load_clean_data(file_loc, columns, 'sleeve')
-	survey = np.array([[6, 76, 185]])
-	ratio_eval_sleeve2 = model_evaluation(linear, X_train, y_train, y_test, X_test, survey)
+	ridge_eval_sleeve2 = ridge_lasso(ridge, X_train, y_train, y_test, X_test)
+	lasso_eval_sleeve2 = ridge_lasso(lasso, X_train, y_train, y_test, X_test)
 
+	'''
+	Chest
+	'''
+	columns = ['jacket_size', 'Slim', 'Very Slim', 'chest']
+	df, X_train, X_test, y_train, y_test = load_clean_data(file_loc, columns, 'chest')
+	ratio_eval_chest = model_evaluation(linear, X_train, y_train, y_test, X_test)
+
+  	ridge_eval_chest = ridge_lasso(ridge, X_train, y_train, y_test, X_test)
+	lasso_eval_chest = ridge_lasso(lasso, X_train, y_train, y_test, X_test)
+
+	columns = ['t_shirt_size', 'height_inches', 'weight_pounds', 'age_years', 'Fit', 'Full', 'Muscular', 'Slim', 'Very Slim', 'chest']
+	df, X_train, X_test, y_train, y_test = load_clean_data(file_loc, columns, 'chest')
+	ratio_eval_chest2 = model_evaluation(linear, X_train, y_train, y_test, X_test)
+
+	ridge_eval_chest2 = ridge_lasso(ridge, X_train, y_train, y_test, X_test)
+	lasso_eval_chest2 = ridge_lasso(lasso, X_train, y_train, y_test, X_test)
+
+	'''
+	Waist
+	'''
+	columns = ['Fit', 'Full', 'Muscular', 'waist']
+	df, X_train, X_test, y_train, y_test = load_clean_data(file_loc, columns, 'waist')
+	ratio_eval_waist = model_evaluation(linear, X_train, y_train, y_test, X_test)
+
+	ridge_eval_chest = ridge_lasso(ridge, X_train, y_train, y_test, X_test)
+	lasso_eval_chest = ridge_lasso(lasso, X_train, y_train, y_test, X_test)
 
 
 # required fields: age, height, weight, tshirt size
-# 12 target variables
+# 12 target variables-->concentrate on neck, sleeve, chest, waist
 
-# if statements for when they supply different information:
-	# if they supply shirt_neck_inches, we don't predict neck
-# incoporating measurements from store
-# build=None=Average/suitlength=None=0/tuck=None=0/fit=None=Slim/Tailored
+# build table of MSE for 3 different models and for 2 different response cats.
+# sklearn.utlis resample  ((ix, iy) for i in xrange(1000))
+
+#what if i did something like just include all the relevant columns and drop columns that aren't relevant?
+# if statements for when they supply different information?
+# incorporating measurements from store?
