08c2894af0f6fa4d760a0fe636a13920b77721a8
khanzlik <knhanzlik@gmail.com>
2015-12-15 14:43:03 -0700

    created retention df

--- ratio_eda.py
+++ ratio_eda.py
@@ -3,6 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+def clean_data(file_loc):
+	df = pd.read_csv(file_loc)
+	drop_columns = ['created_at', 'updated_at', 'source', 'birthday_month', 'posture', 'watch_wrist', 'watch_size', 'rise_inches', 'pocket_size','lastorderdate', 'shoulder_left', 'shoulder_right', 'shoulder_slope', 'estimated_birth_year', 'button_count', 'button_stance', 'posture_alteration', 'lastorderid', 'back_pleats']
+	df.drop(drop_columns, axis=1, inplace=True)
+
+	fill_columns = ['build', 'jacket_length', 'tuck', 'fit']
+	df = fill_nans(df, fill_columns)
+
+	df = dummies(df, 'build')
+	df = dummies(df, 'sleeve_fit')
+	df = dummies(df, 'tuck')
+	df = dummies(df, 'fit')
+	drop_dummies = ['Average', 'Standard', 'Both', 'Traditional']
+	df.drop(drop_dummies, inplace=True, axis=1)
+
+	return df
+
 def dummies(df, column_list):
 	df = pd.concat([df, pd.get_dummies(df[column_list])], axis=1)
 	df.drop(column_list, inplace=True, axis=1)
@@ -38,33 +55,25 @@ if __name__=='__main__':
 	ratio = pd.read_csv('data/ratio_sizing_data.csv')
 
 	orders  = pd.merge(id_userid, ids, 'left', 'id')
-	repeat_orders = pd.merge(orders, ratio, 'inner', 'user_id')
-	index = repeat_orders.duplicated('user_id').loc
+	total_orders = pd.merge(orders, ratio, 'inner', 'user_id')
+	index = total_orders.duplicated('user_id').loc
 
-	df_duplicates = repeat_orders.ix[repeat_orders.duplicated('user_id'), :]
+	df_duplicates = total_orders.ix[total_orders.duplicated('user_id'), :]
 
 	df_duplicates.to_csv('data/repeat_orders')
-
-	#df = pd.read_csv('data/ratio_sizing_data.csv')
-	df = pd.read_csv('data/repeat_orders')
-	drop_columns = ['created_at', 'updated_at', 'source', 'birthday_month', 'posture', 'watch_wrist', 'watch_size', 'rise_inches', 'pocket_size','lastorderdate', 'shoulder_left', 'shoulder_right', 'shoulder_slope', 'estimated_birth_year', 'button_count', 'button_stance', 'posture_alteration', 'lastorderid', 'back_pleats', 'Unnamed: 0', 'id_x']
-	df.drop(drop_columns, axis=1, inplace=True)
-
-	fill_columns = ['build', 'jacket_length', 'tuck', 'fit']
-	df = fill_nans(df, fill_columns)
-
-	df = dummies(df, 'build')
-	df = dummies(df, 'sleeve_fit')
-	df = dummies(df, 'tuck')
-	df = dummies(df, 'fit')
-	drop_dummies = ['Average', 'Standard', 'Both', 'Traditional']
-	df.drop(drop_dummies, inplace=True, axis=1)
-
+	file_loc = 'data/repeat_orders'
+	df = clean_data(file_loc)
 	df.to_csv('data/cleaned_ratio_data')
 
 	# df.describe()
-
 	column_list = ['height_inches', 'age_years', 'weight_pounds']
 	histogram(df, column_list, bins=50)
 
+	'''
+	Retention
+	'''
+	total_orders.to_csv('data/total_orders')
+	file_loc_retention = 'data/total_orders'
+	df_retention = clean_data(file_loc_retention)
+	df.to_csv('data/cleaned_total_orders')
 
