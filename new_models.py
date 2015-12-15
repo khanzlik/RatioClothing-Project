7044a222a643439e4be5ac24a12143b56c3d1ae5
khanzlik <knhanzlik@gmail.com>
2015-12-15 12:25:28 -0700

    made tables of mse/r2

--- new_models.py
+++ new_models.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 from sklearn import linear_model
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import mean_squared_error as mse
@@ -92,39 +91,33 @@ def print_table(file_loc, target):
 	ratio_eval, ridge_eval, lasso_eval, rf, gb = make_model(file_loc, target)
 	mses = [ratio_eval[0], ridge_eval[0], lasso_eval[0], rf[0], gb[0]]
 	r2s = [ratio_eval[1], ridge_eval[1], lasso_eval[1], rf[1], gb[1]]
-	print 'Model\t\t|\tMSE\t\t|\tR2'
+	print '{} Model\t|\tMSE\t\t|\tR2'.format(target)
 	for i, model in enumerate(models):
 		if len(model) > 7:
 			print '{0}\t|\t{1:.4f}\t\t|\t{2:.4f}'.format(model, mses[i], r2s[i])
 		else:
 			print '{0}\t\t|\t{1:.4f}\t\t|\t{2:.4f}'.format(model, mses[i], r2s[i])
 
-
 if __name__ == '__main__':
 
 	file_loc = 'data/cleaned_ratio_data'
 
-	ratio_eval_neck, ridge_eval_neck, lasso_eval_neck, rf_neck, gb_neck = make_model(file_loc, 'neck')
-	# plot_feature_importance(file_loc, 'neck', 'random_forest')
-	# plot_feature_importance(file_loc, 'neck', 'gradient')
+	neck_eval = print_table(file_loc, 'neck')
+	neck_rf_feat = plot_feature_importance(file_loc, 'neck', 'random_forest')
+	neck_gb_feat = plot_feature_importance(file_loc, 'neck', 'gradient')
 	search = grid_search(file_loc, 'neck')
-	# different best params for each target value
-	print_table(file_loc, 'neck')
 
-	ratio_eval_sleeve, ridge_eval_sleeve, lasso_eval_sleeve, rf_sleeve, gb_sleeve = make_model(file_loc, 'sleeve')
-	# results.append(ratio_eval_sleeve, ridge_eval_sleeve, lasso_eval_sleeve, rf_sleeve, gb_sleeve)
-	# plot_feature_importance(file_loc, 'sleeve', 'random_forest')
-	# plot_feature_importance(file_loc, 'sleeve', 'gradient')
+	# sleeve_eval = print_table(file_loc, 'sleeve')
+	# sleeve_rf_feat = plot_feature_importance(file_loc, 'sleeve', 'random_forest')
+	# sleeve_gb_feat = plot_feature_importance(file_loc, 'sleeve', 'gradient')
 
-	ratio_eval_chest, ridge_eval_chest, lasso_eval_chest, rf_chest, gb_chest = make_model(file_loc, 'chest')
-	# results.append(ratio_eval_chest, ridge_eval_chest, lasso_eval_chest, rf_chest, gb_chest)
-	# plot_feature_importance(file_loc, 'chest', 'random_forest')
-	# plot_feature_importance(file_loc, 'chest', 'gradient')
+	# chest_eval = print_table(file_loc, 'chest')
+	# chest_rf_feat = plot_feature_importance(file_loc, 'chest', 'random_forest')
+	# chest_gb_feat = plot_feature_importance(file_loc, 'chest', 'gradient')
 
-	ratio_eval_waist, ridge_eval_waist, lasso_eval_waist, rf_waist, gb_waist = make_model(file_loc, 'waist')
-	# results.append(ratio_eval_chest, ridge_eval_chest, lasso_eval_chest, rf_chest, gb_chest)
-	# plot_feature_importance(file_loc, 'waist', 'random_forest')
-	# plot_feature_importance(file_loc, 'waist', 'gradient')
+	# waist_eval = print_table(file_loc, 'waist')
+	# waist_rf_feat = plot_feature_importance(file_loc, 'waist', 'random_forest')
+	# waist_gb_feat = plot_feature_importance(file_loc, 'waist', 'gradient')
 
 
 
