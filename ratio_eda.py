3de00b61930f7eae04d8c2a9c664cf414ca675c9
khanzlik <knhanzlik@gmail.com>
2015-12-16 19:35:40 -0700

    added functions to make dataframes

--- ratio_eda.py
+++ ratio_eda.py
@@ -3,6 +3,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+def make_ratio_df():
+	id_userid = pd.read_csv('data/id_userid.csv')
+	ids = pd.read_csv('data/id.csv')
+	ratio = pd.read_csv('data/ratio_sizing_data.csv')
+
+	orders  = pd.merge(id_userid, ids, 'left', 'id')
+	total_orders = pd.merge(orders, ratio, 'inner', 'user_id')
+	index = total_orders.duplicated('user_id').loc
+
+	df_duplicates = total_orders.ix[total_orders.duplicated('user_id'), :]
+
+	df_duplicates.to_csv('data/repeat_orders')
+	file_loc = 'data/repeat_orders'
+	df = clean_data(file_loc)
+	df.to_csv('data/cleaned_ratio_data')
+
+	return total_orders, df
+
+def make_retention_df(total_orders):
+	total_orders.to_csv('data/total_orders')
+	num_shirt = pd.DataFrame(total_orders.groupby('user_id').size())
+	num_shirt.rename(columns={0: 'retention'}, inplace=True)
+	num_shirt.reset_index(inplace=True)
+	retention = pd.merge(total_orders, num_shirt, 'inner', 'user_id')
+	retention.eval('churn = retention <= 1')
+	retention.churn = retention.churn.astype(int)
+	drop_columns = ['id_y', 'id_x', 'retention']
+	retention.drop(drop_columns, axis=1, inplace=True)
+	retention.drop_duplicates('user_id', inplace=True)
+	retention.to_csv('data/churn')
+	
+	file_loc_churn = 'data/churn'
+	df_retention = clean_data(file_loc_churn)
+	df_retention.to_csv('data/cleaned_churn')
+	return df_retention
+
 def clean_data(file_loc):
 	df = pd.read_csv(file_loc)
 	drop_columns = ['created_at', 'updated_at', 'source', 'birthday_month', 'posture', 'watch_wrist', 'watch_size', 'rise_inches', 'pocket_size','lastorderdate', 'shoulder_left', 'shoulder_right', 'shoulder_slope', 'estimated_birth_year', 'button_count', 'button_stance', 'posture_alteration', 'lastorderid', 'back_pleats']
@@ -50,43 +86,10 @@ def histogram(df, column_list, bins=10):
 
 if __name__=='__main__':
 
-	id_userid = pd.read_csv('data/id_userid.csv')
-	ids = pd.read_csv('data/id.csv')
-	ratio = pd.read_csv('data/ratio_sizing_data.csv')
-
-	orders  = pd.merge(id_userid, ids, 'left', 'id')
-	total_orders = pd.merge(orders, ratio, 'inner', 'user_id')
-	index = total_orders.duplicated('user_id').loc
-
-	df_duplicates = total_orders.ix[total_orders.duplicated('user_id'), :]
-
-	df_duplicates.to_csv('data/repeat_orders')
-	file_loc = 'data/repeat_orders'
-	drop_columns = ['created_at', 'updated_at', 'source', 'birthday_month', 'posture', 'watch_wrist', 'watch_size', 'rise_inches', 'pocket_size','lastorderdate', 'shoulder_left', 'shoulder_right', 'shoulder_slope', 'estimated_birth_year', 'button_count', 'button_stance', 'posture_alteration', 'lastorderid', 'back_pleats']
-	df = clean_data(file_loc)
-	df.to_csv('data/cleaned_ratio_data')
-
-	# df.describe()
+	total_orders, df = make_ratio_df()
+	df.describe()
 	column_list = ['height_inches', 'age_years', 'weight_pounds']
 	histogram(df, column_list, bins=50)
 
-	'''
-	Retention
-	'''
-	total_orders.to_csv('data/total_orders')
-	num_shirt = pd.DataFrame(total_orders.groupby('user_id').size())
-	num_shirt.rename(columns={0: 'retention'}, inplace=True)
-	num_shirt.reset_index(inplace=True)
-	retention = pd.merge(total_orders, num_shirt, 'inner', 'user_id')
-	retention.eval('churn = retention <= 1')
-	retention.churn = retention.churn.astype(int)
-	drop_columns = ['id_y', 'id_x', 'retention']
-	retention.drop(drop_columns, axis=1, inplace=True)
-	retention.drop_duplicates('user_id', inplace=True)
-	retention.to_csv('data/churn')
-	
-	file_loc_churn = 'data/churn'
-	df_retention = clean_data(file_loc_churn)
-	df_retention.to_csv('data/cleaned_churn')
-
+	make_retention_df(total_orders)
 
