942afca87f297819d725686a5fda68c2c70b798f
khanzlik <knhanzlik@gmail.com>
2015-12-18 16:29:33 -0700

    home page skeleton

--- app/ratio_app.py
+++ app/ratio_app.py
@@ -1,17 +1,15 @@
+import os
+import pandas as pd
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def login():
-    if requet.method == 'POST':
+def home():
+	return render_template('submission_form.html')
 
-    return  
-
-@app.route('/path', methods = ['POST'])
-
-@app.route('/more/')
-def more():
-    return render_template('starter_template.html')
+# @app.route('/predictions')
+# def shirt_predictions():
+# 	return render_template('results.html')
 
 if __name__ == '__main__':
  	app.run(host='0.0.0.0', port=8080, debug=True)
