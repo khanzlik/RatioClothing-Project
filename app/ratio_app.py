76be8ae37de1ecedfe09c3a5f8a8cac97b5fe06b
khanzlik <knhanzlik@gmail.com>
2015-12-22 12:20:56 -0700

    dummified build

--- app/ratio_app.py
+++ app/ratio_app.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import pandas as pd
+import numpy as np
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -16,17 +17,28 @@ def home():
 
 @app.route('/predictions', methods=['POST'])
 def shirt_predictions():
-	names = ["height_inches", "weight_pounds", "age_years", "jacket_size", "jacket_length", "shirt_neck_inches", "shirt_sleeve_inches", "pant_waist_inches", "pant_inseam_inches", "t_shirt_size"]
+	names = ["height_inches", "weight_pounds", "age_years", "jacket_size", "jacket_length", "shirt_neck_inches", "shirt_sleeve_inches", "pant_waist_inches", "pant_inseam_inches", "t_shirt_size", "build"]
 	form_info = [float(request.form[name]) for name in names]
-	 # dummify build, form_info += build
+	form_info = np.array(form_info).reshape(1, -1)
+	df = pd.DataFrame(data=form_info, columns=names)
+
+	builds = {1: 'Fit', 2:'Full', 3:'Muscular'}
+	for build in builds.itervalues():
+		df[build] = 0
+
+	for row_idx in df.index.values:
+		for k, build in builds.iteritems():
+			if df.loc[row_idx, 'build'] == k:
+				df.loc[row_idx, build] = 1
+	df.drop('build', axis=1, inplace=True)
 
 	neck, sleeve, chest, waist = load_pickle_obs(['models/neck.pkl', 'models/sleeve.pkl', 'models/chest.pkl', 'models/waist.pkl'])
-	predictions = make_predictions([neck, sleeve, chest, waist], form_info)
+	predictions = make_predictions([neck, sleeve, chest, waist], df.values)
 
-	neck = 'Neck: {}'.format(str(round(predictions[0], 2)))
-	sleeve = 'Sleeve: {}'.format(str(round(predictions[1], 2)))
-	chest = 'Chest: {}'.format(str(round(predictions[2], 2)))
-	waist = 'Waist: {}'.format(str(round(predictions[3], 2)))
+	neck = 'Neck: {} '.format(str(round(predictions[0], 2)))
+	sleeve = 'Sleeve: {} '.format(str(round(predictions[1], 2)))
+	chest = 'Chest: {} '.format(str(round(predictions[2], 2)))
+	waist = 'Waist: {} '.format(str(round(predictions[3], 2)))
 	return neck+sleeve+chest+waist
 
 def load_pickle_obs(file_paths):
